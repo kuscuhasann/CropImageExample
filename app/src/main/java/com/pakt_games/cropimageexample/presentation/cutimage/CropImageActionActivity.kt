eeed4cc25fcdf3a28ef64f7f52a0408af336ca05
Hasan Kuşçu <kuscuhasann97@gmail.com>
2023-04-01 16:42:09 +0300

    added cropImage library

--- app/src/main/java/com/pakt_games/cropimageexample/presentation/cutimage/CropImageActionActivity.kt
+++ app/src/main/java/com/pakt_games/cropimageexample/presentation/cutimage/CropImageActionActivity.kt
@@ -1,15 +1,36 @@
 package com.pakt_games.cropimageexample.presentation.cutimage
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.app.ActivityCompat
+import com.canhub.cropper.CropImageActivity
+import com.canhub.cropper.databinding.CropImageActivityBinding
 import com.pakt_games.cropimageexample.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CropImageActionActivity : AppCompatActivity() {
+internal class CropImageActionActivity : CropImageActivity() {
+
+    companion object {
+        fun start(activity: Activity) {
+            ActivityCompat.startActivity(
+                activity,
+                Intent(activity, CropImageActionActivity::class.java),
+                null,
+            )
+        }
+    }
+
+    private lateinit var binding: CropImageActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = CropImageActivityBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_crop_image_action)
+
+        
     }
 }
